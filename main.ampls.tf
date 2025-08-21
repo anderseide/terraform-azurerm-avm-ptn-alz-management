6693d28eb99e24f3984cd5d256310c9e67cbb017
Anders Eide <anders.eide@outlook.com>
2025-08-21 21:05:04 +0200

    Trying to fix

--- main.ampls.tf
+++ main.ampls.tf
@@ -11,7 +11,7 @@ resource "azurerm_monitor_private_link_scope" "azure_monitor_private_link_scope"
   count = var.azure_monitor_private_link_scope_enabled ? 1 : 0
 
   name = var.azure_monitor_private_link_scope_name
-  resource_group_name = azurerm_resource_group.azure_monitor_private_link_scope.name
+  resource_group_name = azurerm_resource_group.azure_monitor_private_link_scope[0].name
   tags = var.tags
 
   ingestion_access_mode = var.azure_monitor_private_link_scope_ingestion_access_mode
